0401005dc5c25ee3d45af9cc5b8af705bb42b03f
Mark Alexander <git@markalexander.se>
2018-06-26 18:17:42 +0100

    Add basic data subpackage file structure

---
+++ imbalanced/data/test/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+This package contains data providers and various utilities.
+"""
+
+# Allow for shorter import statements of the form
+#     from imbalanced.data import DatasetName
+# rather than
+#     from imbalanced.data.datasets import DatasetName
+from .datasets import *
