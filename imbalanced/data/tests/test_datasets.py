b0610e92c1071ec9339dae351109a30ed7acf679
Mark Alexander <git@markalexander.se>
2018-06-29 11:18:44 +0100

    Add masked dataset

--- imbalanced/data/tests/test_datasets.py
+++ imbalanced/data/tests/test_datasets.py
@@ -2,7 +2,7 @@
 
 import pytest
 import numpy as np
-from ..datasets import SimpleDataset
+from ..datasets import SimpleDataset, MaskedDataset
 
 
 class TestSimpleDataset:
@@ -47,3 +47,56 @@ class TestSimpleDataset:
         targets = np.random.rand(*targets_shape)
         d = SimpleDataset(inputs, targets)
         return d, inputs, targets
+
+
+class TestMaskedDataset:
+
+    def test_masked_indexes_correctly(self):
+        mask = np.array([True, False, True, False, True], dtype=np.bool)
+        d_masked, d_orig = self.make_random(
+            inputs_shape=(5, 5),
+            targets_shape=(5, 5),
+            mask=mask)
+        assert len(d_masked) == 3
+        i_masked = 0
+        for i_orig in range(len(d_orig)):
+            if mask[i_orig]:
+                assert (d_orig[i_orig][0] == d_masked[i_masked][0]).all()
+                assert (d_orig[i_orig][1] == d_masked[i_masked][1]).all()
+                i_masked += 1
+
+    def test_all_rows_active_by_default(self):
+        d_masked, d_orig = self.make_random()
+        assert len(d_masked) == len(d_orig)
+        for i in range(len(d_orig)):
+            assert (d_orig[i][0] == d_masked[i][0]).all()
+            assert (d_orig[i][1] == d_masked[i][1]).all()
+
+    def test_rejects_invalid_mask(self):
+        # Not an ndarray
+        with pytest.raises(AssertionError):
+            self.make_random(mask=1)
+        # Not the right length
+        with pytest.raises(AssertionError):
+            self.make_random(inputs_shape=(2, 5), targets_shape=(2, 5),
+                             mask=np.array([True, False, True], dtype=np.bool))
+        # Right length but not overall shape
+        with pytest.raises(AssertionError):
+            self.make_random(inputs_shape=(2, 5), targets_shape=(2, 5),
+                             mask=np.array([[True, False], [True, False]],
+                                           dtype=np.bool))
+
+    @staticmethod
+    def make_random(inputs_shape=(10, 5), targets_shape=(10, 5), mask=None):
+        """Create a randomly MaskedDataset object.
+
+        :param inputs_shape:   desired shape for `inputs` tensor
+        :param targets_shape:  desired shape for `targets` tensor
+        :param mask:           mask to be passed to the constructor
+        :return:               masked dataset, original dataset
+        """
+        inputs = np.random.rand(*inputs_shape)
+        targets = np.random.rand(*targets_shape)
+        d = SimpleDataset(inputs, targets)
+        m = MaskedDataset(d, mask)
+        return m, d
