e7f29fead02e5765a6261c546e5f32a09108b5a7
Mark Alexander <git@markalexander.se>
2018-07-04 09:41:32 +0100

    Add simple LA tests

--- imbalanced/tests/test_learner.py
+++ imbalanced/tests/test_learner.py
@@ -6,29 +6,22 @@ from torch.nn.modules.loss import _Loss
 from ..learner import LearningAlgorithm
 
 
-# class DummyOptimizer(Optimizer):
-#     def __init__(self):
-#         pass
-#
-#
-# class DummyCriterion(_Loss):
-#     def __init__(self):
-#         pass
-#
-#
-# class TestLearningAlgorithm:
-#
-#     def test_rejects_invalid_elements(self):
-#         criterion = DummyCriterion()
-#         optimizer = DummyOptimizer()
-#         # Constructor
-#         with pytest.raises(AssertionError):
-#             LearningAlgorithm(1, optimizer)
-#         with pytest.raises(AssertionError):
-#             LearningAlgorithm(criterion, 1)
-#         # Getter/setter
-#         la = LearningAlgorithm(criterion, optimizer)
-#         with pytest.raises(AssertionError):
-#             la.criterion = 1
-#         with pytest.raises(AssertionError):
-#             la.optimizer = 1
+class DummyOptimizer(Optimizer):
+    def __init__(self) -> None:
+        pass
+
+
+class DummyCriterion(_Loss):
+    def __init__(self) -> None:
+        pass
+
+
+class TestLearningAlgorithm:
+
+    def test_rejects_invalid_elements(self) -> None:
+        criterion = DummyCriterion()
+        optimizer = DummyOptimizer()
+        with pytest.raises(AssertionError):
+            LearningAlgorithm(1, optimizer)
+        with pytest.raises(AssertionError):
+            LearningAlgorithm(criterion, 1)
