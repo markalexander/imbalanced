20275648e1dc76abd1123eb2221128f95faf480e
Mark Alexander <git@markalexander.se>
2018-08-03 13:04:12 +0100

    Add feature filtering imbalancer

--- imbalanced/imblancers.py
+++ imbalanced/imblancers.py
@@ -8,14 +8,91 @@ imbalanced, through a variety of methods.  The new object can then be used as a
 dataset in its own right.
 """
 
-from abc import abstractmethod
+from typing import Tuple, Any, Dict, List, Optional
+import torch
 from torch.utils.data import Dataset
-from .datasets import ResampledDataset
+from imbalanced.datasets import DatasetWrapper
 
 
-class Imbalancer:
-    """Base class for all imbalancers."""
+class InflatedFeatureFilteredDataset(DatasetWrapper):
+    """Wrapper which inflates a given dataset to have an increased number of
+    target values of a particular value, e.g. zero.  Selects which features
+    to set to this target value based on the values of input features.
 
-    @abstractmethod
-    def imbalance(self, dataset: Dataset) -> ResampledDataset:
-        pass
+    For example, in a house price prediction dataset we might want to set every
+    row whose 'location' feature is not within a particular region, say 'spain'.
+    All rows whose location feature is not spain would then be set to zero, and
+    importantly this feature column would be removed in output.
+    """
+
+    def __init__(self, dataset: Dataset, filters: Dict[int, Any],
+                 inflated_target: Any = 0.0,
+                 codet_features: Optional[List[int]] = None,
+                 match_type: Any = all) -> None:
+        """Create an InflatedFeatureFilteredDataset object.
+
+        Args:
+            dataset:         The dataset to be inflated.
+            filters:         The filters by which the data are inflated.  As a
+                             dictionary specifying {feature_idx: filter_value}.
+                             Then, any row that matches on all features
+                             specified will retain its normal value.  All other
+                             rows will have their target values replaced with
+                             the inflated target value given.  Note that any
+                             feature specified here will also be removed.
+            inflated_target: The new target value to give those rows that don't
+                             pass the filter.
+            codet_features:  Other co-determined features to remove when
+                             returning rows. For when we filter on some feature
+                             that is also determined by other features that we
+                             are not filtering by, and we would like to remove
+                             these features too.  E.g. in a one-hot
+                             representation.
+            match_type:      Whether passing the filter should mean matching any
+                             feature value or all feature values.  Pass one of
+                             the Python built-ins `any` or `all`.
+
+        Returns:
+            None
+
+        """
+        super().__init__(dataset)
+        self.filters = filters
+        self.inflated_target = inflated_target
+        if codet_features is None:
+            codet_features = []
+        self.match_type = match_type
+        # Determine which features to keep in the rows
+        all_features = set(range(len(self.dataset[0][0])))
+        removed_features = set(
+            codet_features + list(self.filters.keys()))
+        self.keep_features = torch.LongTensor(
+            list(all_features - removed_features))
+
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        """Get a data row by index.
+
+        Args:
+            idx: The index of the desired row.
+
+        Returns:
+            The desired row (input, target)
+
+        """
+        features, target = self.dataset[idx]
+        # Get target value
+        passes_filter = self.match_type([v == features[k]
+                                         for k, v in self.filters.items()])
+        if not passes_filter:
+            target = self.inflated_target
+        # Return with correct features removed
+        return features.index_select(0, self.keep_features), target
+
+    def __len__(self) -> int:
+        """Get the total number of rows in the dataset.
+
+        Returns:
+            The number of rows.
+
+        """
+        return len(self.dataset)
--- imbalanced/tests/test_imbalancers.py
+++ imbalanced/tests/test_imbalancers.py
@@ -1,1 +1,81 @@
 # -*- coding: utf-8 -*-
+
+import torch
+from torch.utils.data import TensorDataset
+from ..imblancers import InflatedFeatureFilteredDataset
+
+
+original_inputs = torch.Tensor([
+    [1, 1, 3, 4],
+    [1, 1, 8, 7],
+    [3, 2, 1, 0],
+    [9, 1, 1, 1],
+    [3, 2, 1, 4],
+    [9, 2, 9, 3]
+])
+
+original_targets = torch.Tensor([
+    1,
+    2,
+    3,
+    4,
+    5,
+    6
+])
+
+
+class TestInflatedFeatureFilteredDataset:
+    """Tests for the InflatedFeatureFilteredDataset class."""
+
+    def test_match_al_target_is_changedl(self) -> None:
+        """Tests whether the target is changed for an 'all' feature match."""
+        dataset = InflatedFeatureFilteredDataset(
+            TensorDataset(original_inputs, original_targets),
+            {1: 2, 2: 1},
+            match_type=all
+        )
+        # Check those that do pass filter keep original values
+        for idx in [2, 4]:
+            assert original_targets[idx] == dataset[idx][1]
+        # Check those that don't pass filter are zeroed out
+        for idx in [0, 1, 3, 5]:
+            assert 0 == dataset[idx][1]
+
+    def test_match_or_target_is_changed(self) -> None:
+        """Tests whether the target is changed for an 'any' feature match."""
+        dataset = InflatedFeatureFilteredDataset(
+            TensorDataset(original_inputs, original_targets),
+            {1: 2, 2: 1},
+            match_type=any
+        )
+        # Check those that do pass filter keep original values
+        for idx in [2, 3, 4, 5]:
+            assert original_targets[idx] == dataset[idx][1]
+        # Check those that don't pass filter are zeroed out
+        for idx in [0, 1]:
+            assert 0 == dataset[idx][1]
+
+    def test_filter_features_are_removed(self) -> None:
+        """Tests whether the features used for filtering are then removed
+        from the features given for each row."""
+        dataset = InflatedFeatureFilteredDataset(
+            TensorDataset(original_inputs, original_targets),
+            {1: 2}
+        )
+        for idx in range(len(dataset)):
+            inputs, target = dataset[idx]
+            assert original_inputs[idx][0] == inputs[0]
+            assert original_inputs[idx][2] == inputs[1]
+            assert original_inputs[idx][3] == inputs[2]
+
+    def test_additional_features_are_removed(self) -> None:
+        """Tests whether the given co-determined features are also removed."""
+        dataset = InflatedFeatureFilteredDataset(
+            TensorDataset(original_inputs, original_targets),
+            {1: 2},
+            codet_features=[2]
+        )
+        for idx in range(len(dataset)):
+            inputs, target = dataset[idx]
+            assert original_inputs[idx][0] == inputs[0]
+            assert original_inputs[idx][3] == inputs[1]
